b1bbcbd47edc59c377f4da1537320f92d34b7739
Jeremy Albright <myrlin1@gmail.com>
2020-02-04 19:52:49 -1000

    error handling & walk up realpath for node_modules

--- src/gatsby-node.ts
+++ src/gatsby-node.ts
@@ -1,7 +1,7 @@
 import * as path from 'path';
 import { Configuration } from 'webpack';
 import { GatsbyNode, CreateWebpackConfigArgs, PluginOptions } from 'gatsby';
-import { realpath, isDir, fileExists } from './utils';
+import { realpath, isDir, fileExists, walkBack } from './utils';
 
 interface IPnpmOptions extends PluginOptions {
     resolutions: string[];
@@ -20,15 +20,20 @@ interface IPnpmOptions extends PluginOptions {
  * |:----------|:------------|
  * | resolutions  |a list of package names and/or paths that you would like to be made available to Webpack.  Each of these should either be the name of one of your project's direct dependencies, or a path to a folder containing packages that can be resolved as a module.|
  */
-export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = async ({ actions }: CreateWebpackConfigArgs, options: IPnpmOptions): Promise<void> => {
+export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = async ({ actions, reporter }: CreateWebpackConfigArgs, options: IPnpmOptions): Promise<void> => {
     const { setWebpackConfig } = actions;
     const nodeModules = path.join(process.cwd(), 'node_modules');
     const pnpmNodeModules = path.join(nodeModules, '.pnpm', 'node_modules');
-    const gatsbyNodeModules = path.dirname(await realpath(path.join(nodeModules, 'gatsby')));
+    const gatsbyNodeModules = await walkBack(await realpath(path.join(nodeModules, 'gatsby')));
 
-    const modulePaths = [
-        gatsbyNodeModules,
+    if (!gatsbyNodeModules) {
+        throw new Error("[gatsby-plugin-pnpm] Unable to resolve your Gatsby install's real path!!");
+    }
+
+    const modulePaths: string[] = [
+        'node_modules',
         nodeModules,
+        gatsbyNodeModules,
         pnpmNodeModules,
     ];
 
@@ -41,11 +46,15 @@ export const onCreateWebpackConfig: GatsbyNode['onCreateWebpackConfig'] = async 
             } else {
                 // We need to check if the option is a valid dependency of the
                 // current project
-                const nodePath = path.resolve(path.join(nodeModules, pkgName));
+                const pkgPath = path.resolve(path.join(nodeModules, pkgName));
                 // If the resolved package name exists in the current project's
                 // node_modules, then push its realpath.
-                if (await fileExists(nodePath)) {
-                    modulePaths.push(await realpath(nodePath));
+                if (await fileExists(pkgPath)) {
+                    const nodePath = await walkBack(await realpath(pkgPath));
+                    if (nodePath) modulePaths.push(nodePath);
+                    if (!nodePath) {
+                        reporter.warn(`[gatsby-plugin-pnpm] Unable to locate dependency ${pkgName}'s node_modules from its real path`);
+                    }
                 }
             }
         }
